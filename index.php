009905fd2abe767b4acd7ef71a7021fd771ea193
marsya <marsya210709@gmail.com>
2026-02-09 15:04:13 +0700

    coba

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// Program Hello Word Pertamaku !
+// Dibuat oleh: Marsya
+
+echo "Halo, Nama Saya Marsya";
+echo "<br>";  // <br> adalah tag HTML untuk baris baru
+echo "saya Siswa SMK Kelas 2";
+echo "<br>";
+echo "Jurusan Rekayasa Perangkat Lunak";
+
+?>
---
+++ waktu.php
@@ -0,0 +1,15 @@
+<?php
+// Program menampilkan tanggal dan waktu
+
+echo " Selamat Datang!  ";
+echo "<br>";
+echo "Hari ini tanggal: 09-02-2026";
+echo date ("d-m-Y"); // Format: 09-02-2026
+echo "<br>";
+echo "Jam sekarang: ";
+echo date ("H:i:s"); // Format: 14:16:06
+echo "<br>";
+echo "<br>";
+echo "Semoga harimu menyenangkan!???  ";
+
+?>
